a81a3ae2b89744b7cdbe26f6c3e1b84c05475cc0
ZEFF <evandrogoetz5@gmail.com>
2024-05-26 01:49:20 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -41,3 +41,60 @@ backend WebSocket_back
     server server2 127.0.0.1:78 check
     server server3 127.0.0.1:79 check
 " > /tmp/arquivo.cfg
+
+mkdir /etc/SSHPlus
+mkdir /opt/sshplus
+echo > /opt/sshplus/sshplus
+cd /etc/SSHPlus/ && wget https://github.com/zeff0669/cleancore/raw/main/WebSocket && wget https://github.com/zeff0669/cleancore/raw/main/pub.key && wget https://github.com/zeff0669/cleancore/raw/main/priv.pem && chmod +X WebSocket && cd -
+
+echo "
+[Unit]
+Description=WebSocket Service
+After=network.target
+
+[Service]
+ExecStart=/etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:77 -listem_port 127.0.0.1:22 -msg=CleanCoreWS1
+TasksMax=infinity
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/websocket.service
+echo "
+[Unit]
+Description=WebSocket Service
+After=network.target
+
+[Service]
+ExecStart=/etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:78 -listem_port 127.0.0.1:22 -msg=CleanCoreWS2
+TasksMax=infinity
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/websocket2.service
+echo "
+[Unit]
+Description=WebSocket Service
+After=network.target
+
+[Service]
+ExecStart=/etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:79 -listem_port 127.0.0.1:22 -msg=CleanCoreWS3
+TasksMax=infinity
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/websocket3.service
+
+systemctl daemon-reload
+systemctl enable websocket.service
+systemctl enable websocket1.service
+systemctl enable websocket2.service
+systemctl start websocket.service
+systemctl start websocket1.service
+systemctl start websocket2.service
+systemctl restart haproxy
+
+
+
